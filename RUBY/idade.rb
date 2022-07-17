28702030f948f58e568adc8c813b8e24d990fbbb
Esdrasborges <105466934+Esdrasborges@users.noreply.github.com>
2022-07-17 15:29:29 -0300

    inicio em ruby

---
+++ RUBY/idade.rb
@@ -0,0 +1,15 @@
+puts "Qual o seu nome?"
+nome = gets 
+puts "Em que ano nos estamos?"
+ano = gets.to_i
+puts "Em que ano voce nasceu?"
+nasci = gets.to_i
+
+idade = ano - nasci
+puts "voce tem #{idade}" 
+
+if idade >= 18 
+    puts "Voce ja pode da entrada na sua CNH"
+else
+    puts "Voce é de menor, ainda não tem autorização"
+end
